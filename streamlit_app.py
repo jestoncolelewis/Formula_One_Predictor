fd83591e876f874a9d21200d10bf0ac17c416603
Jeston Cole Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2024-08-05 14:52:35 +0000

    added cacheing and refined output

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,26 +2,36 @@ import streamlit as st
 import pandas as pd
 import ydf
 
-st.write("Jeston Lewis - Capstone Project")
 
-data = pd.read_csv("./data/final_rolling.csv")
+st.title("Jeston Lewis - Capstone Project")
+
+@st.cache_data
+def build_data():
+    data = pd.read_csv("./data/final_rolling.csv")
+    data.dropna(subset="position", inplace=True)
+    data["position"] = data["position"].astype("int")
+    return data
+
+@st.cache_resource
+def build_model():
+    return ydf.RandomForestLearner(label="position").train(training[predictors])
+
 predictors = ["grid", "position", "pos_delta", "driver_code", "constructor_code", "circuit_code", "grid_rolling", "position_rolling", "pos_delta_rolling"]
-data.dropna(subset="position", inplace=True)
-data["position"] = data["position"].astype("int")
-
+data = build_data()
 training = data[data["year"] < 2022]
 test = data[data["year"] >= 2022]
 
-model = ydf.RandomForestLearner(label="position").train(training[predictors])
+model = build_model()
 
 predictions = model.predict(test[predictors])
 predictions_df = pd.DataFrame(predictions)
 
 evaluation = model.evaluate(test[predictors])
 
-st.write(f"test accuracy {evaluation.accuracy}")
+st.header(f"Test accuracy - {evaluation.accuracy:.2f}%")
 
 full_table = pd.merge(test, predictions_df, on=test.index)
 full_table["max_pred"] = full_table[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]].max(axis=1)
 
-st.table(full_table[["raceId", "driverRef", "constructorRef", "position", "circuitRef", "max_pred"]])
+single = full_table[["raceId", "driverRef", "constructorRef", "position", "circuitRef", "max_pred"]].loc[full_table["raceId"] == 1134]
+st.table(single)
