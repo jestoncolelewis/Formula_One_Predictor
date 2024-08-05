90f26ccd60a191d1b8b1c59abe54781c6294bd1d
Jeston Cole Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2024-08-05 13:29:37 +0000

    started building app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,26 @@ import streamlit as st
 import pandas as pd
 import ydf
 
-st.write("Lewis Jeston - Capstone Project")
+st.write("Jeston Lewis - Capstone Project")
 
-dataset_df = pd.read_csv("./data/final_rolling.csv")
+data = pd.read_csv("./data/final_rolling.csv")
+predictors = ["grid", "position", "pos_delta", "driver_code", "constructor_code", "circuit_code", "grid_rolling", "position_rolling", "pos_delta_rolling"]
+data.dropna(subset="position", inplace=True)
+data["position"] = data["position"].astype("int")
 
-training = dataset_df[dataset_df["raceId"] < 2022]
-test = dataset_df[dataset_df["year"] >= 2022]
+training = data[data["year"] < 2022]
+test = data[data["year"] >= 2022]
 
-model = ydf.RandomForestLearner(label="position").train(training)
+model = ydf.RandomForestLearner(label="position").train(training[predictors])
 
-st.write(model.predict(test))
+predictions = model.predict(test[predictors])
+predictions_df = pd.DataFrame(predictions)
+
+evaluation = model.evaluate(test[predictors])
+
+st.write(f"test accuracy {evaluation.accuracy}")
+
+full_table = pd.merge(test, predictions_df, on=test.index)
+full_table["max_pred"] = full_table[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]].max(axis=1)
+
+st.table(full_table[["raceId", "driverRef", "constructorRef", "position", "circuitRef", "max_pred"]])
