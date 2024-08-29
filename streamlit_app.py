eb1bc23a0a36ca7459556ac0a946226609fc9c8f
Jeston Lews <61254261+jestoncolelewis@users.noreply.github.com>
2024-08-29 15:18:23 +0200

    removed max pred column

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,12 +47,10 @@ def build_viz(_local_model, local_data, _local_inspector):
 
 
 def make_prediction(local_model, local_data):
-    # TODO: remove max
     local_data_ds = tfdf.keras.pd_dataframe_to_tf_dataset(local_data[predictors])
     predictions = local_model.predict(local_data_ds)
     predictions_df = pd.DataFrame(predictions)
     local_table = pd.merge(local_data, predictions_df, on=local_data.index)
-    local_table["max_pred"] = local_table[preds].max(axis=1)
     return local_table
 
 
