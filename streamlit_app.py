62ba37d7ea6e05ed53cb482e4601451207df2f37
Jeston Lews <61254261+jestoncolelewis@users.noreply.github.com>
2024-08-29 13:10:17 +0200

    reformatted table

--- streamlit_app.py
+++ streamlit_app.py
@@ -170,7 +170,12 @@ plt.ylabel("Logloss (out-of-bag)")
 st.title("Jeston Lewis - Capstone Project")
 tab1, tab2 = st.tabs(["Analysis", "Predictor"])
 tab1.header(f"Test accuracy - {eval_perc:.2f}%")
-tab1.dataframe(single.style.format({1:"{:.2%}"}), use_container_width=True, hide_index=True) # Single race prediction
+tab1.dataframe(
+    single.style.format({1:"{:.2%}"}),
+    use_container_width=True,
+    hide_index=True,
+    height=738,
+) # Single race prediction
 tab1.pyplot(plt) # Plot logs
 tab1.image(viz, use_column_width=True) # Plot tree
 
