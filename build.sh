053bc5c095712e9fb0763154a24df461d764341b
Jeston Lews <61254261+jestoncolelewis@users.noreply.github.com>
2024-09-04 18:43:21 +0200

    platform and updated port specifications

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ if [ $# -eq 0 ];
     tag=$1
 fi
 
-docker image build --platform linux/amd64 -t jestoncolelewis/f1-predictor:$tag .
-docker run -p 8080:8080 f1-predictor:$tag
+docker image build -t jestoncolelewis/f1-predictor:$tag .
+docker run -p 80:8051 f1-predictor:$tag
