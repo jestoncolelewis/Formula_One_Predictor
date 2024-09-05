e80eda0ab86a8f04374a25ac01a96ae919a24d66
Jeston Lews <61254261+jestoncolelewis@users.noreply.github.com>
2024-09-05 15:41:13 +0200

    removed options in model build

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ def load_model(path):
 @st.cache_resource
 def build_model(local_training):
     train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(local_training[predictors], label="position")
-    local_model = tfdf.keras.RandomForestModel(task=tfdf.keras.core.Task.CLASSIFICATION,verbose=0)
+    local_model = tfdf.keras.RandomForestModel()
     local_model.fit(train_ds)
     local_model.save("./model")
     return local_model
