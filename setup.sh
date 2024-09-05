2f02e30e600fcd246ddbae481649450df0529744
Jeston Lews <61254261+jestoncolelewis@users.noreply.github.com>
2024-09-05 15:49:06 +0200

    removed python version

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 python3 -m pip install virtualenv
-virtualenv .venv --python=python3.11
+virtualenv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
